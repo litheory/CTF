1e20d3e97a1e784ef71dabea7f57092dfed43b9a
乃逸夫 <676098269@qq.com>
2017-11-14 20:33:52 +0800

    凯撒密码

---
+++ crypt/caeser.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# coding=utf-8
+#__author__ = 'tyomcat'
+
+def convert(c, key, start = 'a', n = 26):
+    a = ord(start)
+    offset = ((ord(c) - a + key)%n)
+    return chr(a + offset)
+def caesarEncode(s, key):
+    o = ""
+    for c in s:
+        if c.islower():
+            o+= convert(c, key, 'a')
+        elif c.isupper():
+            o+= convert(c, key, 'A')
+        else:
+            o+= c
+    return o
+def caesarDecode(s, key):
+    return caesarEncode(s, -key)
+if __name__ == '__main__':
+
+    for key in range(27):
+       e='l1cg{faslaicas1irh3cpb}csis3'     #写这里
+       d = caesarDecode(e, key)
+
+       print d
+       print '\n'
